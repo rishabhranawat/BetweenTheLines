fd0b84f49082ab2f79e952d846994e43e2c56a25
rishabhranawat <rishabhranawat12345@gmail.com>
2017-01-18 18:01:43 +0530

    changes

--- TwiNet/twi_get.py
+++ TwiNet/twi_get.py
@@ -4,9 +4,5 @@ auth = tweepy.OAuthHandler("7ORmqhp1lXxpazSzpTGVVNAv6", "KXwjMsUcf0cKLIZ94D76GYw
 auth.set_access_token("3530340917-urj3UuRWL6frj2FtdhQjj6v0PpVF0PoJy6hp6og", "eW9ZyEZID417pFtZDTLwUI95u60TCcict290qxnBIXleE")
 
 api = tweepy.API(auth)
-user = api.get_user('realDonaldTrump')
-
-
-public_tweets = user.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+statuses = api.user_timeline('realDonaldTrump', count = 200)
+print(statuses[0].text)
