c6f3f7721900f6860ae493cb8a9c15142b4074d2
rishabhranawat <rishabhranawat12345@gmail.com>
2017-07-09 10:41:02 -0400

    started wtih movies text extraction

--- crowdSourcing/redditTrial.py
+++ crowdSourcing/redditTrial.py
@@ -1,17 +1,24 @@
 import praw
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 reddit = praw.Reddit(client_id='eR6bB9c2Juu6mQ',
                      client_secret='uW4z9EaCIfFbbexRBS1WpGsHB2s',
                      user_agent='my user agent')
 
-# for submission in reddit.subreddit('learnpython').hot(limit=10):
-#     print(submission.title)
+URL = "https://www.reddit.com/r/movies/comments/4i35uf/\
+official_discussion_captain_america_civil_war/?st=j4w491jj&sh=22ea1167"
 
-URL = 'https://www.reddit.com/r/AskReddit/comments/688ik4/teachers_of_reddit_what_is_wrong_with_modern/?st=j4csvnr9&sh=ba977b59'
 submission = reddit.submission(url=URL)
-
+top_level_comments = []
 for top_level_comment in submission.comments:
 	try: 
 		for comment in top_level_comment.replies:
-			print(comment.body)
+			top_level_comments.append(comment.body)
 	except: pass
+
+cv = CountVectorizer()
+cv_fit=cv.fit_transform(top_level_comments)
+
+print(cv.get_feature_names())
+print(cv_fit.toarray())
